cbf9931747bcad5cb9c7671daccd2bf0f3e27467
5aliveR6 <63644885+5aliveR6@users.noreply.github.com>
2022-04-18 19:08:14 +0200

    Added database options, command modal enhancements

--- main.ts
+++ main.ts
@@ -6,16 +6,19 @@ interface MyPluginSettings {
 	searchString: string;
 	settingsBool: boolean;
 	filelocation: string;
+	preferredDB: string;
 }
 
 const savedSettings: MyPluginSettings = {
 	searchString: 'Maier, Michael',
 	settingsBool: true,
-	filelocation: '/Personen/'
+	filelocation: '/Personen/',
+	preferredDB: 'ndb'
 }
 
 
 const vaultaccess = app.vault;
+const fileaccess = FileSystemAdapter;
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -23,23 +26,13 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// // This creates an icon in the left ribbon.
-		// const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-		// 	// Called when the user clicks the icon.
-		// 	new Notice('This is a notice!');
-		// });
-		// // Perform additional things with the ribbon
-		// ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		// const statusBarItemEl = this.addStatusBarItem();
-		// statusBarItemEl.setText('Status Bar Text');
-
-		// THis adds a simple command for lookup.
+		// Main Command for Lookup Historical Data
 		this.addCommand({
 			id: 'ndb-lookup',
-			name: 'Lookup a historical figure on NDB',
+			name: 'Start a Query for historical biological data',
 			callback: () => {
+
+				// Fetch the data from the NDB API.
 				async function getData() {
 					const response = await requestUrl({
 						url: "http://data.deutsche-biographie.de/beta/solr-open/?q=defnam:%22" + savedSettings.searchString + "%22&wt=json",
@@ -49,67 +42,25 @@ export default class MyPlugin extends Plugin {
 					console.log(response.json.response.docs[1].r_leb_str);
 					console.log(response.json.response.docs[1].defnam);
 				}
-				getData();
 
+				// Display a modal UI Element to enter the search string and update savedSettings.searchString
+				new LookUpModal(this.app, (result) => {
+					//check if result is not null, if not null, update saved.Settings.searchstring
+					if (result != null) {
+						savedSettings.searchString = result;
+						this.saveSettings();
+						getData();
+					} else {
+						new Notice('Please enter a name.');
+					}
+				  }).open();
 
-				// //check if folder exists, else create it
-				// async function savelocationcheck() {
-				// 	const folderExists = await FileSystemAdapter.exists(savedSettings.filelocation)
-				
-				// }
-				
-				
-				// {
-				// 	async function writeData() {
-				// 		await vaultaccess.createFolder(
-				// 			savedSettings.filelocation,
-				// 			);
-				// 	}
-				// 	writeData();
-				
+			
 			}
 			
 		});
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-				
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		// Add the settings tab entry for this plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
@@ -135,36 +86,53 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class LookUpModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  result: string;
+  onSubmit: (result: string) => void;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Insert Name');
-	}
+  constructor(app: App, onSubmit: (result: string) => void) {
+    super(app);
+    this.onSubmit = onSubmit;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  onOpen() {
+    const { contentEl } = this;
+
+    contentEl.createEl("h1", { text: "Start Historical Query" });
+
+    new Setting(contentEl)
+	.setName("Preferred Database").setDesc("Which Database should be set as default?").addDropdown((d) => {
+		d.addOption("ndb", "Neue Deutsche Biographie");
+		d.addOption("bitlib", "British Library");
+		d.addOption("met", "Metropolitan Museum of Art");
+		d.setValue(savedSettings.preferredDB);
+		d.onChange((v) => 
+		  savedSettings.preferredDB = v);})
+
+
+
+    new Setting(contentEl)
+      .setName("Lastname, Firstname")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.result = value
+        }));
+
+    new Setting(contentEl)
+      .addButton((btn) =>
+        btn
+          .setButtonText("Search")
+          .setCta()
+          .onClick(() => {
+            this.close();
+            this.onSubmit(this.result);
+          }));
+  }
+
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
@@ -180,7 +148,16 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Deutsche Biographie Lookup'});
+		containerEl.createEl('h2', {text: 'Historical Query'});
+
+		new Setting(containerEl)
+		.setName("Preferred Database").setDesc("Which Database should be set as default?").addDropdown((d) => {
+			d.addOption("ndb", "Neue Deutsche Biographie");
+			d.addOption("bitlib", "British Library");
+			d.addOption("met", "Metropolitan Museum of Art");
+			d.setValue(savedSettings.preferredDB);
+			d.onChange((v) => 
+			  savedSettings.preferredDB = v);})
 
 		new Setting(containerEl)
 			.setName('Name for Testing Purposes')
@@ -216,6 +193,15 @@ class SampleSettingTab extends PluginSettingTab {
 				this.plugin.settings.settingsBool = value;
 				await this.plugin.saveSettings();
 			}));
+
+			const hltrDonationDiv = containerEl.createEl("div", {
+				cls: "hltrDonationSection",
+			});
+			const addendum = createEl("p");
+			addendum.appendText("This plugin is free to use. The data provided by the databases are subject to their respective licenses.");
+			hltrDonationDiv.appendChild(addendum);	
 }
 }
+
+
 
